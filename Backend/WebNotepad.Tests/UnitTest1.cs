7fde0e6b0425e753a0e49f3e872485283922abe6
xsnailu <xsnailu@gmail.com>
2021-03-26 11:46:45 +0100

    tests init

---
+++ Backend/WebNotepad.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebNotepad.Models;
+
+namespace WebNotepad.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [ClassInitialize]
+        public static void CreateDB(TestContext testContext)
+        {
+            var options = new DbContextOptionsBuilder<WebNotepadDBContext>()
+               .UseInMemoryDatabase(databaseName: "WebNotepadDB")
+               .Options;
+            using var context = new WebNotepadDBContext(options);
+            context.CurrentNotes.Add(new CurrentNote { Id = 1, Title = "Tytul1", Content = "Content1", Created = DateTime.Now, Modified = DateTime.Now, IsActive = true });
+            context.CurrentNotes.Add(new CurrentNote { Id = 2, Title = "Tytul2", Content = "Content2", Created = DateTime.Now, Modified = DateTime.Now, IsActive = true });
+            context.CurrentNotes.Add(new CurrentNote { Id = 3, Title = "Tytul3", Content = "Content3", Created = DateTime.Now, Modified = DateTime.Now, IsActive = true });
+            context.ArchiveNotes.Add(new ArchiveNote { NoteId = 3, Title = "Tytul3", Content = "Content3", Created = DateTime.MinValue, Modified = DateTime.MinValue, IsActive = true });
+            context.SaveChanges();
+        }
+    }
+}
